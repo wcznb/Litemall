600270ff7724b179ba99c951c9fbad8a87a42fb7
cosinec <1726697408@qq.com>
2020-12-04 21:03:19 +0800

    用户模块更新

---
+++ userservice/src/main/java/cn/edu/xmu/user/service/UserService.java
@@ -0,0 +1,38 @@
+package cn.edu.xmu.user.service;
+
+import cn.edu.xmu.ooad.model.VoObject;
+import cn.edu.xmu.ooad.util.ReturnObject;
+import cn.edu.xmu.user.model.vo.CustomerSetVo;
+import cn.edu.xmu.user.model.vo.NewUserVo;
+import com.github.pagehelper.PageInfo;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public interface UserService {
+    @Transactional
+    ReturnObject register(NewUserVo vo);
+
+    @Transactional
+    ReturnObject<String> login(String userName, String password, String ipAddr);
+
+    void banJwt(String jwt);
+
+    ReturnObject Logout(Long userId);
+
+    ReturnObject<PageInfo<VoObject>> getallusers(String userName, String email, String mobile, Integer page, Integer pageSize);
+
+    @Transactional
+    ReturnObject<Object> banCustomer(Long userId);
+
+    @Transactional
+    ReturnObject<Object> releaseCustomer(Long userId);
+
+    @Transactional
+    ReturnObject <Object> getCustomer(Long id);
+
+    ReturnObject <Object> modifyCustomer(Long id, CustomerSetVo vo);
+
+    @Transactional
+    ReturnObject<VoObject> findUserById(Long id);
+}
